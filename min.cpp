2cbdcf2e13a7f921e2b42a8c7ee189a5a96db316
Jonathan <jhii2@outlook.com.au>
2025-05-18 12:27:16 +1000

    Update edit data base feature

--- min.cpp
+++ min.cpp
@@ -95,44 +95,87 @@ void search(student s[])
         cout << "\n\n Data Base is Empty...";
 }
 
+void update(student s[])
+{
+    system("cls");
+    string roll;
+    int count = 0;
+    if (i > 0)
+    {
+        cout << "\n\n\t\t\t\tUpdate Record";
+        if (i > 0)
+        {
+            cout << "\n\n Roll No. For Update : ";
+            cin >> roll;
+            for (int a = 0; a < i; a++)
+            {
+                if (roll == s[a].roll)
+                {
+                    cout << "\n\n New Roll No. : ";
+                    cin >> s[a].roll;
+                    cout << "\n\t\tName : ";
+                    cin >> s[a].name;
+                    cout << "\n Class : ";
+                    cin >> s[a].clas;
+                    cout << "\n\t\tTotal Marks : ";
+                    cin >> s[a].tot;
+                    cout << "\n Obtained Marks : ";
+                    cin >> s[a].obt;
+                    s[i].per = (s[i].obt / s[i].tot) * 100;
+                    count++;
+                }
+            }
+        }
+        if (count == 0)
+        {
+            cout << "\n\n Record Not Found....";
+        }
+    }
+    else
+    {
+        cout << "\n\n Database is Empty....";
+    }
+}
+
 int main()
 {
     int choice;
     char x;
-p:
-    system("cls"); // clear console screen
-    control_panel();
-    cin >> choice; // inputs
-    switch (choice)
+    while (true) // replaces goto and label
     {
-    case 1:
-        do
+        system("cls"); // clear console screen
+        control_panel();
+        cin >> choice; // inputs
+        switch (choice)
         {
-            insert(s);
-            i++; // increase array index
-            cout << "\n\n Do You Want To Add Another Record (y,n) : ";
-            cin >> x;
-        } while (x == 'y');
-        break;
+        case 1:
+            do
+            {
+                insert(s);
+                i++; // increase array index
+                cout << "\n\n Do You Want To Add Another Record (y,n) : ";
+                cin >> x;
+            } while (x == 'y');
+            break;
 
-    case 2:
-        display(s);
-        break;
-    case 3:
-        search(s);
-        break;
-    case 4:
-        // Code to update record
-        break;
-    case 5:
-        // Code to delete record
-        break;
-    case 6:
-        exit(0);
-    default:
-        cout << "\n\n Invalid Value...Please Try Again...";
+        case 2:
+            display(s);
+            break;
+        case 3:
+            search(s);
+            break;
+        case 4:
+            update(s);
+            break;
+        case 5:
+            // Code to delete record
+            break;
+        case 6:
+            exit(0);
+        default:
+            cout << "\n\n Invalid Value...Please Try Again...";
+        }
+
+        getch();
     }
-
-    getch();
-    goto p; // loop jumps to p:
 }
